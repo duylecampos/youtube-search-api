b344080e123e8dd02941fccdd78884977f5656e0
Duyle Campos <duylecampos@gmail.com>
2020-06-07 19:39:10 -0300

    fix(tests): YouTubeMovie must receive thumbnail parameter

--- tests/services/youtube/test_collections.py
+++ tests/services/youtube/test_collections.py
@@ -13,6 +13,7 @@ from app.services.youtube.models import YoutubeMovie
                 "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus porta volutpat ante id tristique",
                 "Suspendisse sed suscipit sapien. Mauris lobortis accumsan est, et lacinia velit feugiat ultrices. Mauris et urna non felis aliquet consectetur. Sed ullamcorper in est at feugiat. Aliquam erat volutpat. Aliquam aliquet laoreet volutpat. Nunc a sem interdum, sollicitudin elit et, gravida ex. Aliquam tincidunt lobortis libero, sit amet dictum velit placerat sit amet. Quisque semper nibh nisi, vel pharetra est porta imperdiet.",
                 "PT6M39S",
+                "https://i.ytimg.com/vi/WRKYfRPTcy0/mqdefault.jpg",
             )
         ]
     ],
@@ -50,7 +51,9 @@ def test_common_words(movies):
     ],
 )
 def test_days_to_watch_all(movies_time, availability, days_spent):
-    movies = [YoutubeMovie(None, None, None, duration) for duration in movies_time]
+    movies = [
+        YoutubeMovie(None, None, None, duration, None) for duration in movies_time
+    ]
     c = YoutubeCollection(movies)
     days = c.days_to_watch_all(availability)
     assert days == days_spent
